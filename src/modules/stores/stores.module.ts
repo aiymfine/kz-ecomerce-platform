53987a913ddbbb0455968c2b7df956afaa007527
Aiymfine <aiymfine@gmail.com>
2026-04-24 16:26:38 +0500

    fix: add JwtModule import to all modules that use JwtAuthGuard

--- src/modules/stores/stores.module.ts
+++ src/modules/stores/stores.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { StoresController } from './stores.controller';
 import { StoresService } from './stores.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          algorithm: configService.get<string>('JWT_ALGORITHM') as 'HS256',
+        },
+      }),
+    }),
+  ],
   controllers: [StoresController],
   providers: [StoresService],
   exports: [StoresService],
